ba7db5de5dadf62d70d4f3ff1af30c4cd88973cc
Vedant0370 <vedant.0370@gmail.com>
2024-01-17 15:49:44 +0530

    update ViewEmployerPackage.js and MonthlyPackages.js

--- admin/src/Components/EmployerPackege/ViewEmployerPackage.js
+++ admin/src/Components/EmployerPackege/ViewEmployerPackage.js
@@ -6,9 +6,25 @@ import Footer from "../Footer/Footer";
 
 const ViewEmployerPackages = () => {
   const [employers, setEmployers] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [candidateDetails, setCandidateDetails] = useState(null);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+   
+};
+
+const closeModal = () => {
+    setIsModalOpen(false);
+};
+  
+const handleViewMoreClick = (candidate) => {
+  openModal();
+  // Set detailed information for the modal
+  setCandidateDetails(candidate);
+};
 
   useEffect(() => {
-    // Fetch employer data from the API
     const fetchEmployers = async () => {
       try {
         const response = await fetch("http://localhost:8000/api/employer");
@@ -23,14 +39,12 @@ const ViewEmployerPackages = () => {
   }, []);
 
   const handlePendingClick = async (employerId) => {
-    // Confirm with the admin
     const isConfirmed = window.confirm('Are you sure you want to accept this request?');
 
     if (!isConfirmed) {
-      return; // If not confirmed, do nothing
+      return;
     }
 
-    // Make a PATCH request to update paymentStatus to "Accepted"
     const apiUrl = `http://localhost:8000/api/employer/${employerId}`;
     const data = { paymentStatus: 'Accepted' };
 
@@ -44,20 +58,13 @@ const ViewEmployerPackages = () => {
       });
 
       if (response.ok) {
-        // Successfully updated paymentStatus, perform any other necessary actions
         console.log('PaymentStatus updated to Accepted');
-
-        // You can force a re-render or update the state to immediately reflect the change
-        // For example, if employers is the state containing the employer data:
-        // setEmployers(updatedEmployers);
 
       } else {
         console.error('Failed to update paymentStatus');
-        // Handle the error as needed
       }
     } catch (error) {
       console.error('Error during PATCH request:', error);
-      // Handle the error as needed
     }
   };
   return (
@@ -101,49 +108,84 @@ const ViewEmployerPackages = () => {
                     </tr>
                   </thead>
                   <tbody>
-                 
-                  {employers
-  .filter(
-    (employer) =>
-      employer.purchacepackageDate && employer.purchacepackageDate.trim() !== "" // Check if purchacepackageDate is not empty
-  )
-  .map((employer) => (
-  <tr key={employer._id}>
-    <td className="py-2 px-4 border-b text-lg">{employer.email}</td>
-    <td className="py-2 px-4 border-b text-lg">{employer.empName}</td>
-    <td className="py-2 px-4 border-b text-lg">{/* Display package details */}</td>
-    <td className="py-2 px-4 border-b text-lg">
-      <button className="text-blue-500 hover:text-blue-700 mr-2">
-        View More
-      </button>
-    </td>
-    <td>
-    <button
-            onClick={() => handlePendingClick(employer._id)}
-            className={`${
-              employer.paymentStatus === 'Accepted'
-                ? "bg-green-500"
-                : employer.paymentStatus === 'Pending'
-                  ? "bg-red-500"
-                  : "bg-gray-500" // Default color if status is neither Accepted nor Pending
-            } p-2 rounded-lg text-white`}
-          >
-            {employer.paymentStatus ? employer.paymentStatus : "Pending"}
-          </button>
-    </td>
-  </tr>
-))}
+
+                    {employers
+                      .filter(
+                        (employer) =>
+                          employer.purchacepackageDate && employer.purchacepackageDate.trim() !== ""
+                      )
+                      .map((employer) => (
+                        <tr key={employer._id}>
+                          <td className="py-2 px-4 border-b text-lg">{employer.email}</td>
+                          <td className="py-2 px-4 border-b text-lg">{employer.empName}</td>
+                          <td className="py-2 px-4 border-b text-lg"></td>
+                          <td className="py-2 px-4 border-b text-lg">
+                            <button
+                            onClick={() => handleViewMoreClick(employer)}
+                             className="text-blue-500 hover:text-blue-700 mr-2">
+                              View More
+                            </button>
+                          </td>
+                          <td>
+                            <button
+                              onClick={() => handlePendingClick(employer._id)}
+                              className={`${employer.paymentStatus === 'Accepted'
+                                  ? "bg-green-500"
+                                  : employer.paymentStatus === 'Pending'
+                                    ? "bg-red-500"
+                                    : "bg-gray-500"
+                                } p-2 rounded-lg text-white`}
+                            >
+                              {employer.paymentStatus ? employer.paymentStatus : "Pending"}
+                            </button>
+                          </td>
+                        </tr>
+                      ))}
                   </tbody>
                 </table>
               </div>
 
               <div className="flex justify-center">
-                {/* Additional content */}
+
               </div>
             </div>
           </div>
         </div>
       </div>
+
+
+      {isModalOpen && (
+  <div className="fixed inset-0 bg-gray-700 bg-opacity-50 flex items-center justify-center">
+    <div className="bg-white p-8 max-w-md w-full rounded-md">
+      <h2 className="text-xl font-semibold mb-4">Student Details</h2>
+      {/* Display detailed information here */}
+      <div>
+        <label className="block mb-2">First Name: {candidateDetails.empName}</label><hr/>
+        <label>Email: {candidateDetails.email}</label><hr/>
+        <label>number: {candidateDetails.number}</label><hr/>
+        <label>company Address: {candidateDetails.companyAddress}</label><hr/>
+        <label>purchacepackageDate: {candidateDetails.purchacepackageDate}</label><hr/>
+        <label>purchacepackageEndDate: {candidateDetails.purchacepackageEndDate}</label><hr/>
+        <label>packagePrice: {candidateDetails.packagePrice}</label><hr/>
+        <label>accountHolderName: {candidateDetails.accountHolderName}</label><hr/>
+        <label>paymentStatus: {candidateDetails.searches}</label><hr/>
+        <label>Remeaning internshipEnquiry: {candidateDetails.internshipEnquiry}</label><hr/>
+        <label>verifiedApplication: {candidateDetails.verifiedApplication}</label><hr/>
+        <label>ResumeView: {candidateDetails.ResumeView}</label><hr/>
+        <label>dedicatedCRM: {candidateDetails.dedicatedCRM}</label><hr/>
+        
+      </div>
+      <div className="flex justify-between mt-3">
+        <button
+          className="text-black bg-gray-300 px-4 py-2 rounded-md hover:bg-gray-500"
+          onClick={closeModal}
+        >
+          Cancel
+        </button>
+      </div>
+    </div>
+  </div>
+)}
 
       <Footer />
     </>
