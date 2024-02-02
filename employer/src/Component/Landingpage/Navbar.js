cf4aeca82a9f3b4b54b2188314f2ef913003f71f
AishK26 <aishwaryakumbhar8767@gmail.com>
2024-02-02 11:51:34 +0530

    update Interns_bee_combination-removebg-preview.png and Navbar.js

--- employer/src/Component/Landingpage/Navbar.js
+++ employer/src/Component/Landingpage/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from 'react';
-import logo from "../../Assets/Internsb.jpeg"
+import logo from "../../Assets/Interns_bee_combination-removebg-preview.png"
 import { Link, useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
@@ -64,8 +64,8 @@ const Navbar = () => {
         <div className="mb-10">
             <div className="navbar-container fixed top-0 left-0 w-full z-50 bg-white shadow-md p-4 flex items-center justify-between border">
                 <div className="flex items-center ">
-                    <img src={logo} alt="Logo" className="w-14 h-14 rounded-full" />
-                    <h1 className="text-4xl font-bold heading-in-navbar">Interns <span className="text-4xl font-bold text-amber-300">Bee</span></h1>
+                    <img src={logo} alt="Logo" className="w-15 h-14 rounded-full" />
+                    {/* <h1 className="text-4xl font-bold heading-in-navbar">Interns <span className="text-4xl font-bold text-amber-300">Bee</span></h1> */}
                 </div>
                 {/* <div className="items-center space-x-6">
                     <a href="#" className="text-2xl font-bold focus:text-yellow-300 focus:border-yellow-300 focus:border-b-4">Home</a>
