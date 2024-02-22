d07cc20987a91fed6b7dcb7c5f1c036c8fbf8d18
AishK26 <aishwaryakumbhar8767@gmail.com>
2024-02-22 23:27:28 +0530

    update App.js, PostInternship.js and Home1.js

--- students/src/Components/Home1.js
+++ students/src/Components/Home1.js
@@ -10,6 +10,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { FiUser } from "react-icons/fi";
+
 import {
   FaCalendar,
   FaMoneyBill,
