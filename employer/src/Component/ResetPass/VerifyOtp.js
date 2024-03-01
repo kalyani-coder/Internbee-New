db9b7041b850df914dbd694dbd5e5bd08e9b29b6
Imakashkendre <akash.shopisthan@gmail.com>
2024-03-01 17:14:33 +0530

    update RestPass.js and VerifyOtp.js

--- employer/src/Component/ResetPass/VerifyOtp.js
+++ employer/src/Component/ResetPass/VerifyOtp.js
@@ -25,7 +25,7 @@ const VerifyOtp = () => {
       );
 
       setMessage(response.data.message);
-      alert("success");
+      alert("Password Changed Successfully");
       navigate("/login");
     } catch (error) {
       console.error("Error:", error.response.data);
@@ -93,9 +93,9 @@ const VerifyOtp = () => {
 
                 <button
                   type="submit"
-                  className="w-full text-black bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                  className="w-full text-black bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 border border-black"
                 >
-                  Submit
+                  Reset Password
                 </button>
               </form>
 
