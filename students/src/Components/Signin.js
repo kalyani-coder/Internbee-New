26b1f6a1b1a2e3cf2ae2aedf9a69f20b3e659170
Imakashkendre <akash.shopisthan@gmail.com>
2024-03-01 13:58:08 +0530

    update Signin.js

--- students/src/Components/Signin.js
+++ students/src/Components/Signin.js
@@ -14,6 +14,9 @@ const Login = () => {
     formState: { errors },
   } = useForm();
   const [apiError, setApiError] = useState(null);
+  const [showForgotPasswordPopup, setShowForgotPasswordPopup] = useState(false);
+  const [showResetPasswordPopup, setShowResetPasswordPopup] = useState(false);
+  const [showResetButton, setShowResetButton] = useState(false);
 
   useEffect(() => {
     const storedToken = localStorage.getItem("token");
@@ -65,7 +68,62 @@ const Login = () => {
       // Handle other errors, show an error message, etc.
     }
   };
+  const handleSavePasswordClick = async (data) => {
+    try {
+      const response = await fetch(
+        "https://backend.internsbee.com/api/employer/reset-password",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        }
+      );
 
+      if (response.ok) {
+        setShowResetPasswordPopup(false);
+        // Show success alert or navigate to another page
+      } else {
+        const errorData = await response.json();
+        console.error("Error saving new password:", errorData.error);
+      }
+    } catch (error) {
+      console.error("Error saving new password:", error);
+    }
+  };
+
+  const handleForgotPasswordClick = () => {
+    setShowForgotPasswordPopup(true);
+  };
+
+  const closeForgotPasswordPopup = () => {
+    setShowForgotPasswordPopup(false);
+  };
+  const handleSendOtpClick = async (data) => {
+    try {
+      const response = await fetch(
+        "https://backend.internsbee.com/api/employer/send-otp",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        }
+      );
+
+      if (response.ok) {
+        closeForgotPasswordPopup();
+        setShowResetPasswordPopup(true); // Update state to show the Reset Password form
+      } else {
+        const errorData = await response.json();
+        setApiError(errorData.error);
+      }
+    } catch (error) {
+      console.error("Error sending OTP:", error);
+    }
+  };
   return (
     <>
       <div className="">
@@ -168,9 +226,12 @@ const Login = () => {
                     </div>
                   </div>
                   <a
+                    onClick={handleForgotPasswordClick}
                     href="#"
-                    className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
+                    className="text-sm font-medium text-primary-600
+                    hover:underline dark:text-primary-500"
                   >
+                    {" "}
                     Forgot password?
                   </a>
                 </div>
@@ -198,6 +259,132 @@ const Login = () => {
           </div>
         </div>
       </div>
+
+      {showForgotPasswordPopup && (
+        <div className="fixed inset-0 bg-gray-800 bg-opacity-90 z-50 flex items-center justify-center">
+          <div className="bg-white rounded-lg shadow-md p-6 space-y-4">
+            <button
+              onClick={closeForgotPasswordPopup}
+              className="absolute top-4 right-4 text-gray-500 hover:text-gray-700 focus:outline-none"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+            <h1 className="text-lg font-bold leading-tight tracking-tight text-black">
+              Forgot Password
+            </h1>
+            <form
+              onSubmit={handleSubmit(handleSendOtpClick)}
+              className="space-y-4"
+            >
+              <div>
+                <label
+                  htmlFor="forgotEmail"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Registered Email
+                </label>
+                <input
+                  type="email"
+                  name="forgotEmail"
+                  id="forgotEmail"
+                  {...register("forgotEmail", {
+                    required: "Email is required",
+                  })}
+                  className={`bg-gray-50 border ${
+                    errors.forgotEmail ? "border-red-500" : "border-black"
+                  } text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-white dark:border-black dark:placeholder-gray-700 dark:text-black dark:focus:ring-black dark:focus:border-black`}
+                  placeholder="name@company.com"
+                  required
+                />
+                {errors.forgotEmail && (
+                  <p className="text-red-500">{errors.forgotEmail.message}</p>
+                )}
+              </div>
+              {!showResetButton && (
+                <button
+                  type="button"
+                  onClick={() => {
+                    // Handle sending OTP logic here
+                    handleSendOtpClick();
+                    setShowResetButton(true);
+                  }}
+                  className={`w-full text-black bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 border border-black`}
+                >
+                  Send OTP
+                </button>
+              )}
+
+              {showResetButton && (
+                <>
+                  <div className="mb-2">
+                    <label className="block mb-2 text-sm font-medium text-gray-900">
+                      New Password
+                    </label>
+                    <input
+                      type="password"
+                      className={`bg-gray-50 border ${
+                        errors.forgotEmail ? "border-red-500" : "border-black"
+                      } text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-white dark:border-black dark:placeholder-gray-700 dark:text-black dark:focus:ring-black dark:focus:border-black`}
+                      required
+                    />
+                    {errors.forgotEmail && (
+                      <p className="text-red-500">
+                        {errors.forgotEmail.message}
+                      </p>
+                    )}
+                  </div>
+                  <label
+                    htmlFor="otp"
+                    className="block mb-2 text-sm font-medium text-gray-900"
+                  >
+                    Enter OTP
+                  </label>
+                  <input
+                    type="text"
+                    name="otp"
+                    id="otp"
+                    {...register("otp", { required: "OTP is required" })}
+                    className={`bg-gray-50 border ${
+                      errors.otp ? "border-red-500" : "border-black"
+                    } text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-white dark:border-black dark:placeholder-gray-700 dark:text-black dark:focus:ring-black dark:focus:border-black`}
+                    placeholder="Enter OTP"
+                    required
+                  />
+                  {errors.otp && (
+                    <p className="text-red-500">{errors.otp.message}</p>
+                  )}
+                </>
+              )}
+
+              {showResetButton && (
+                <button
+                  type="button"
+                  onClick={() => {
+                    // Handle resetting password logic here
+                    handleSavePasswordClick();
+                  }}
+                  className={`mt-4 w-full text-black bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 border border-black`}
+                >
+                  Reset Password
+                </button>
+              )}
+            </form>
+          </div>
+        </div>
+      )}
     </>
   );
 };
